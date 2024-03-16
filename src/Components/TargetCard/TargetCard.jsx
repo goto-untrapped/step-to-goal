313f4e33f32556b393f42283515ef38847312482
goto-untrapped <pocket_vcalneiatry@yahoo.co.jp>
2024-03-16 18:13:40 +0900

    initial commit

---
+++ src/Components/TargetCard/TargetCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const TargetCard = () => {
+    return (
+        <div class="targetCard">
+            <div class="targetArea">
+                <div class="targetTitle">目標</div>
+                <div class="targetContent">XXXX</div>
+            </div>
+            <div class="todoArea">
+                <div class="todoTitle">TODO</div>
+                <div class="todoContent">YYYY</div>
+                <div class="todoContent">YYYY</div>
+                <div class="todoContent">YYYY</div>
+            </div>
+        </div>
+    )
+}
+
+export default TargetCard
